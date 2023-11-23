c80de5acad0e7dc9e31d453c15127f80495c09c8
CK_Rainbow <nanashichi@proton.me>
2023-11-23 11:49:32 +0800

    fix: update pipeline

--- pipeline.bat
+++ pipeline.bat
@@ -4,7 +4,7 @@ git submodule update
 :: if the submodule folder "liliths-throne-public" exists, discard all the changes to it
 if exist liliths-throne-public (
 	git submodule foreach git reset --hard
-	git submodule foreach git clean -xd
+	git submodule foreach git clean -df
 )
 
 python ./main.py --no-update-repo
--- pipeline.sh
+++ pipeline.sh
@@ -6,7 +6,7 @@ git submodule update
 # if the submodule folder "liliths-throne-public" exists, discard all the changes to it
 if [ -d "liliths-throne-public" ]; then
 	git submodule foreach git reset --hard
-	git submodule foreach git clean -xd
+	git submodule foreach git clean -df
 fi
 
 python ./main.py --no-update-repo
