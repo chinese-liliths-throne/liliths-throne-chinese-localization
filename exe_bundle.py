b7ce3e5d457b10c52d4a1fbbb5967bf53ea2e716
CKRainbow <nanashichi@proton.me>
2024-04-03 00:28:06 +0800

    fix: launch4j file version fix

--- exe_bundle.py
+++ exe_bundle.py
@@ -23,6 +23,15 @@ def add_plugin(pom_path: Path, plugin_path: Path):
     namespace = pom_tree.getroot().nsmap
 
     pom_plugins = list(pom_tree.iterfind("//plugins", namespaces=namespace))
+    
+    # find version
+    plugin_version = list(pom_tree.iterfind("//version", namespaces=namespace))[0].text
+
+    padding = max(0, 3 - len(plugin_version.split('.')))
+    plugin_version = f"${{project.version}}{''.join(['.0'] * padding)}"
+    
+    plugin_tree.getroot().find(".//fileVersion").text = plugin_version
+    plugin_tree.getroot().find(".//productVersion").text = plugin_version
 
     if len(pom_plugins) > 0:
         pom_plugins[0].append(plugin_tree.getroot())
