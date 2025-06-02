632534103697cbab1b1c622c46d76432d43447d0
CKRainbow <nanashichi@proton.me>
2025-06-02 14:15:33 +0800

    update

--- update.py
+++ update.py
@@ -114,7 +114,7 @@ class Updater:
             prev_outdated_data = {}
 
         if len(outdated_data) > 0:
-            _, prev_outdated_data = await self.update_data(outdated_data, prev_outdated_data, version="0.4.10.0")
+            _, prev_outdated_data = await self.update_data(outdated_data, prev_outdated_data, version="0.4.10.7")
         else:
             prev_outdated_data = outdated_data
         
